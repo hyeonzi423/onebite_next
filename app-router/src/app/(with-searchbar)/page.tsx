43a1d5a13161785b88b14c8991bd14747bf7d5fb
김현지 <chik1y@naver.com>
2024-11-11 14:04:06 +0900

    3.5 리액트 서버 컴포넌트 주의사항

--- app-router/src/app/(with-searchbar)/page.tsx
+++ app-router/src/app/(with-searchbar)/page.tsx
@@ -1,5 +1,21 @@
+import ClientComponent from "./client-component";
 import styles from "./page.module.css";
+import ServerComponent from "./server-component";
 
 export default function Home() {
-  return <div className={styles.page}>인덱스 페이지</div>;
+  return (
+    <div className={styles.page}>
+      인덱스 페이지
+      <ClientComponent>
+        <ServerComponent />
+      </ClientComponent>
+    </div>
+  );
 }
+
+/*
+1. 서버 컴포넌트에는 브라우저에서 실행될 코드가 포함되면 안된다.
+2. 클라이언트 컴포넌트는 클라이언트에서만 실행되지 않는다.
+3. 클라이언트 컴포넌트에서 서버 컴포넌트를 import 할 수 없다.
+4. 서버 컴포넌트에서 클라이언트 컴포넌트에게 직렬화 되지 않는 Props는 전달 불가하다.
+*/
