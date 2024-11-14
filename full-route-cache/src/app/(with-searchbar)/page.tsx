4d47a2966a20e46960b4032b0b534372e9f65e30
김현지 <chik1y@naver.com>
2024-11-14 09:22:20 +0900

    5.4 라우트 세그먼트 옵션

--- full-route-cache/src/app/(with-searchbar)/page.tsx
+++ full-route-cache/src/app/(with-searchbar)/page.tsx
@@ -2,6 +2,15 @@ import BookItem from "@/components/book-item";
 import style from "./page.module.css";
 import { BookData } from "@/types";
 
+export const dynamic = "auto";
+/*
+특정 페이지의 유형을 강제로 static, dynamic 페이지로 설정
+1. auto : 기본값, 아무것도 강제하지 않음
+2. force-dynamic : 페이지를 강제로 dynamic 페이지로 설정
+3. force-static : 페이지를 강제로 statuc 페이지로 설정
+4. error : 페이지를 강제로 statuc 페이지로 설정(설정하면 안되는 이유 -> 빌드 오류)
+*/
+
 async function AllBooks() {
   const response = await fetch(`http://localhost:12345/book`, { cache: "force-cache" });
   if (!response.ok) {
